fdcfdbecf3fcaf53a7917b271a17c054c6430d6f
samuel Adeyinka <tboysammy@gmail.com>
2024-06-02 09:48:50 +0100

    update

--- models/state.py
+++ models/state.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import Base, BaseModel
-from sqlalchemy import String, Column, orm
+from sqlalchemy import String, Column
 from sqlalchemy.orm import relationship
 from os import getenv
-import models
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
 
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-o")
-    else:
-        name = ""
+    cities = relationship("City", backref="state", cascade="all, delete-orphan")
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
 
     if getenv('HBNB_TYPE_STORAGE') != 'db':
-
-    @property
-    def cities(self):
-        """getter for cities if the storage engine is not DBStorage"""
+        @property
+        def cities(self):
+            """getter for cities if the storage engine is not DBStorage"""
             from models import storage
             from models.city import City
             city_list = []
             for city in storage.all(City).values():
-                if city.state_id == self.id:
-                    city_list.append(city)
-                except AttributeError
+                try:
+                    if city.state_id == self.id:
+                        city_list.append(city)
+                except AttributeError:
                     pass
             return city_list
